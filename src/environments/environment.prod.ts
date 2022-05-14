5381a01b12c85cf5ab536ae762a6301a24610a9c
Anna Zielińska <ann.ka.zielinska@gmail.com>
2022-05-14 12:33:50 +0100

    feat: firebase connected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'hackathon-team',
+    appId: '1:183321359963:web:5fa6f8ccfc65f54745b17e',
+    storageBucket: 'hackathon-team.appspot.com',
+    apiKey: 'AIzaSyDW5VwweGCo2Bk1PYSCkiGworhKWB3s6ZU',
+    authDomain: 'hackathon-team.firebaseapp.com',
+    messagingSenderId: '183321359963',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'hackathon-team',
+    appId: '1:183321359963:web:5fa6f8ccfc65f54745b17e',
+    storageBucket: 'hackathon-team.appspot.com',
+    apiKey: 'AIzaSyDW5VwweGCo2Bk1PYSCkiGworhKWB3s6ZU',
+    authDomain: 'hackathon-team.firebaseapp.com',
+    messagingSenderId: '183321359963',
+  },
   production: false
 };
 
