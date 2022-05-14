9cf1196647905a5dc43d1fd0e5fd98c3aedd9d15
Michalol94 <michal.olech94@gmail.com>
2022-05-14 14:41:25 +0200

    feat/productsOnHome

--- src/app/pages/home.page-module.ts
+++ src/app/pages/home.page-module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
+import { ProductListComponentModule } from '../../../projects/products/src/lib/adapters/primary/ui/product-list.component-module';
+import { FirebaseProductsServiceModule } from '../../../projects/products/src/lib/adapters/secondary/infrastructure/firebase-products.service-module';
 
-@NgModule({ imports: [CommonModule, 
-      RouterModule.forChild([
-        {
-          path: '',
-          component: HomePage,
-        }
-      ])],
-  	declarations: [HomePage],
-  	providers: [],
-  	exports: [] })
-export class HomePageModule {
-}
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage,
+      },
+    ]),
+    ProductListComponentModule,
+    FirebaseProductsServiceModule,
+  ],
+  declarations: [HomePage],
+  providers: [],
+  exports: [],
+})
+export class HomePageModule {}
