a55a8639eddfe91c700669bb4827710ae87a68da
patorlowska <p.patorlowska@gmail.com>
2022-05-14 17:04:33 +0200

    feat: layout-cart

--- projects/cart/src/lib/adapters/primary/ui/cart-list.component-module.ts
+++ projects/cart/src/lib/adapters/primary/ui/cart-list.component-module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartListComponent } from './cart-list.component';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule],
+@NgModule({ imports: [CommonModule, RouterModule],
   	declarations: [CartListComponent],
   	providers: [],
   	exports: [CartListComponent] })
