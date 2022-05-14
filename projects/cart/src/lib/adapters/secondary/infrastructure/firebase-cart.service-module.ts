86fbcb9b0307271b0b5f32dd52ed71d838d190cd
Michalol94 <michal.olech94@gmail.com>
2022-05-14 17:44:26 +0200

    working

--- projects/cart/src/lib/adapters/secondary/infrastructure/firebase-cart.service-module.ts
+++ projects/cart/src/lib/adapters/secondary/infrastructure/firebase-cart.service-module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { FirebaseCartService } from './firebase-cart.service';
-import { GETS_ALL_CART_DTO } from '../../../application/ports/secondary/gets-all-cart.dto-port';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { FirebaseCartService } from './firebase-cart.service';
 import { GETS_ALL_CART_DTO } from '../../../application/ports/secondary/gets-all-cart.dto-port';
+import { FirebaseCartService } from './firebase-cart.service';
 import { ADDS_CART_DTO } from '../../../application/ports/secondary/adds-cart.dto-port';
 import { REMOVES_CART_DTO } from '../../../application/ports/secondary/removes-cart.dto-port';
 
-@NgModule({ imports: [AngularFirestoreModule],
-  	declarations: [],
+@NgModule({
+  imports: [AngularFirestoreModule],
+  declarations: [],
 
-  	providers: [FirebaseCartService, { provide: GETS_ALL_CART_DTO, useExisting: FirebaseCartService }, { provide: ADDS_CART_DTO, useExisting: FirebaseCartService }, { provide: REMOVES_CART_DTO, useExisting: FirebaseCartService }],
+  providers: [
+    FirebaseCartService,
+    { provide: GETS_ALL_CART_DTO, useExisting: FirebaseCartService },
+    { provide: ADDS_CART_DTO, useExisting: FirebaseCartService },
+    { provide: REMOVES_CART_DTO, useExisting: FirebaseCartService },
+  ],
 
-  	exports: [] })
-export class FirebaseCartServiceModule {
-}
+  exports: [],
+})
+export class FirebaseCartServiceModule {}
