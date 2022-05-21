b75b1a9561b6af40e4619924f825408f2e043ef8
patorlowska <p.patorlowska@gmail.com>
2022-05-21 16:13:48 +0200

    feat:view cart layout

--- projects/cart/src/lib/adapters/primary/ui/cart-list.component-module.ts
+++ projects/cart/src/lib/adapters/primary/ui/cart-list.component-module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartListComponent } from './cart-list.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-@NgModule({ imports: [CommonModule, RouterModule],
+@NgModule({ imports: [CommonModule, RouterModule, ReactiveFormsModule],
   	declarations: [CartListComponent],
   	providers: [],
   	exports: [CartListComponent] })
