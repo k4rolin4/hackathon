bfd75c12e2a3816e03270f00a916b3fbc7b7bd44
k4rolin4 <100521527+k4rolin4@users.noreply.github.com>
2022-05-29 11:36:17 +0200

    feat: Product/Detail (#24)

--- projects/products/src/lib/adapters/primary/ui/product-detail.component-module.ts
+++ projects/products/src/lib/adapters/primary/ui/product-detail.component-module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './product-detail.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
-@NgModule({ imports: [CommonModule],
+@NgModule({ imports: [CommonModule, BsDropdownModule.forRoot(), ModalModule.forRoot()],
   	declarations: [ProductDetailComponent],
   	providers: [],
   	exports: [ProductDetailComponent] })
