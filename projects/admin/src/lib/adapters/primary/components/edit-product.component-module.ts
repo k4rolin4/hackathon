ccbdf9a89ea5e8f4086ee27be6d1d1febf5fbc16
Michalol94 <michal.olech94@gmail.com>
2022-05-30 23:01:42 +0200

    Sorry za kradziez navbara :(

--- projects/admin/src/lib/adapters/primary/components/edit-product.component-module.ts
+++ projects/admin/src/lib/adapters/primary/components/edit-product.component-module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { EditProductComponent } from './edit-product.component';
 
-@NgModule({ imports: [CommonModule],
+@NgModule({ imports: [CommonModule, ReactiveFormsModule],
   	declarations: [EditProductComponent],
   	providers: [],
   	exports: [EditProductComponent] })
--- projects/admin/src/lib/adapters/primary/components/edit-product.component.ts
+++ projects/admin/src/lib/adapters/primary/components/edit-product.component.ts
@@ -4,12 +4,17 @@ import {
   Inject,
   ViewEncapsulation,
 } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { ProductContext } from '../../../application/ports/secondary/context/product.context';
 import {
   SELECTS_PRODUCT_CONTEXT,
   SelectsProductContextPort,
 } from '../../../application/ports/secondary/context/selects-product.context-port';
+import {
+  SETS_PRODUCT_DTO,
+  SetsProductDtoPort,
+} from '../../../application/ports/secondary/dto/sets-product.dto-port';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
@@ -22,9 +27,32 @@ export class EditProductComponent {
   product$: Observable<Partial<ProductContext>> =
     this._selectsProductContext.select();
 
+  readonly editProduct: FormGroup = new FormGroup({
+    name: new FormControl(),
+    price: new FormControl(),
+    desc: new FormControl(),
+    imageUrl: new FormControl(),
+    id: new FormControl(),
+  });
+
   constructor(
     public modalRef: BsModalRef,
     @Inject(SELECTS_PRODUCT_CONTEXT)
-    private _selectsProductContext: SelectsProductContextPort
+    private _selectsProductContext: SelectsProductContextPort,
+    @Inject(SETS_PRODUCT_DTO) private _setsProductDto: SetsProductDtoPort
   ) {}
+
+  onButtonSubmited(editProduct: FormGroup): void {
+    this._setsProductDto
+      .set({
+        name: editProduct.get('name')?.value,
+        desc: editProduct.get('desc')?.value,
+        price: editProduct.get('price')?.value,
+        imageUrl: editProduct.get('imageUrl')?.value,
+        id: editProduct.get('id')?.value,
+      })
+      .subscribe();
+    this.editProduct.reset();
+    console.log(editProduct);
+  }
 }
